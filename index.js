5b0aed2f3fc58831f4fa81974cf0a39931a08128
lbalan90 <lbalan@myseneca.ca>
2022-12-13 16:29:34 -0500

    add my files

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');  // include the "express" module
+const app = express();
+const port = 3000;
+
+// Define route for get request at '/'
+app.get('/', (req, res) => res.send('hello, express world'));
+// Define the static resource (HTML/CSS/JS/images)
+app.use(express.static('public'));             // URL '/' (root) maps to 'public' directory
+app.use('/static', express.static('public'));  // URL '/static' also maps to 'public' directory
+
+app.listen(port, () => console.log(`Server listening at port ${port}...`));
